218f5236a6849147ac75f81d6f30edf74d1e3f08
Cole <cole@colex.dev>
2023-03-10 19:27:58 -0500

    pmm coming together, can find free page

--- kernel/mm/pmm.c
+++ kernel/mm/pmm.c
@@ -3,48 +3,93 @@
 #include "../stdlib/util.h"
 #include "../drivers/tty.h"
 
+/* FIXME: I think all my math is wrong here???? 
+ * FIXME: especially MAX_NUM_OF_PAGES */
+
 /*
- * 4 GiB / 4KiB = 1 MiB
+ * 4 GiB / 4KiB = 1 MiB pages
  * 1 MiB / 8 bytes = 131072 bytes = 0x20000
  */
-#define MAX_NUM_OF_PAGES 0x20000
+#define MAX_NUM_OF_PAGES 0x100000
 /* NOTE: Switching this to 32 may improve performance */
 #define WORD_LENGTH      0x8
+#define SIZE_OF_BITMAP MAX_NUM_OF_PAGES / WORD_LENGTH
 #define KERNEL_START     0x100000 /* 1 MiB */
 #define PAGE_SIZE        0x1000   /* 4 KiB */
 
 uint32_t KERNEL_SIZE     = 1; 
 
 /* Length of page frame will always be 4096 bytes (4KiB) (0x1000) */
-uint8_t bitmap[MAX_NUM_OF_PAGES];
+uint8_t bitmap[SIZE_OF_BITMAP];
 /* 
  *   76543210
- * 1 00000000
- * 2 00000000
+ * 1 00010100
+ * 2 00001001
  *
- * Algo is byte * 8 + bit
+ * Algo is byte * WORD_LENGTH + bit
  * */
 
 void
-set_page(uint8_t bit)
+pmm_set_page(uint8_t page)
 {
-    bitmap[bit / WORD_LENGTH] |= (1 << (bit % WORD_LENGTH));
+    bitmap[page / WORD_LENGTH] |= (1 << (page % WORD_LENGTH));
 }
 
 void
-clear_page(uint8_t bit)
+pmm_clear_page(uint8_t page)
 {
-    bitmap[bit / WORD_LENGTH] &= ~(1 << (bit % WORD_LENGTH));
+    bitmap[page / WORD_LENGTH] &= ~(1 << (page % WORD_LENGTH));
+}
+
+uint8_t
+pmm_get_page(uint8_t page)
+{
+    uint8_t ret = bitmap[page / WORD_LENGTH] & (1 << (page % WORD_LENGTH));
+    return ret != 0;
+}
+
+uint32_t
+pmm_find_free_page()
+{
+    uint8_t offset;
+
+    for (int byte = 0; byte < SIZE_OF_BITMAP; byte++) {
+        if (byte == 255)
+            continue;
+
+        /* Get rightmost 0 bit (free page) */
+        uint8_t num = ~byte & (byte + 1);
+        switch (num) {
+            case 1:
+                offset = 0;
+            case 2:
+                offset = 1;
+            case 4:
+                offset = 2;
+            case 8:
+                offset = 3;
+            case 16:
+                offset = 4;
+            case 32:
+                offset = 5;
+            case 64:
+                offset = 6;
+            case 128:
+                offset = 7;
+        }
+        return byte * WORD_LENGTH + offset;
+    }
+    return 0;
 }
 
 void
-reserve_memory()
+pmm_reserve_memory()
 {
     /* Kernel starts at 0x100000, so we need to reserve
      * up to 0x100000 plus however big the kernel is.
      * CURRENTLY the loop iterates over pages UP TO 0x100000 */
     for (uint32_t page = 0; page < (KERNEL_START / PAGE_SIZE); page++) {
-        set_page(page);
+        pmm_set_page(page);
     }
 
     /* What do I need to reserve all non usable memory? 
@@ -58,4 +103,11 @@ reserve_memory()
      *
      * Based on the above implementation idea I think the easiest way
      * to store the info would be an array of structs */
+}
+
+void
+pmm_init()
+{
+    memset(bitmap, 0, MAX_NUM_OF_PAGES * sizeof(uint8_t));
+    pmm_reserve_memory();
 }
--- kernel/mm/pmm.h
+++ kernel/mm/pmm.h
@@ -2,5 +2,6 @@
 #define PMM_H
 
 #include <stdint.h>
+void pmm_init();
 
 #endif /* #ifndef PMM_H */
